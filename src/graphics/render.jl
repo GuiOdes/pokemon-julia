6f0d72befaf17188f0807b63d436644059e90c3f
HadrianRabelo <hadrianrabelo@hotmail.com>
2023-10-30 19:41:08 -0300

    render starting

---
+++ src/graphics/render.jl
@@ -0,0 +1,15 @@
+using GLVisualize, GeometryTypes, GLAbstraction
+
+# Cria um triângulo
+positions = Point3f0[(0, 0, 0), (1, 0, 0), (0, 1, 0)]
+faces = Face{3}[(1, 2, 3)]
+mesh = GLNormalMesh(Mesh(positions, faces))
+
+# Cria uma janela
+window = glscreen()
+
+# Adiciona o triângulo à janela
+_view(visualize(mesh), window)
+
+# Renderiza a janela
+renderloop(window)
